e289ff781676c14bf13421f4bcb4b1f5271f05f0
ahmed <ahmedhali345@gmail.com>
2022-05-12 10:49:01 +0200

    add search component and add search feature

---
+++ app/components/vue-components/search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="search" v-model="keyword" @input="search()" />
+
+    <button class="search-btn" @click="clearSearch">Clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchComponent",
+  data() {
+    return {
+      keyword: new URLSearchParams(window.location.search).get("filter")
+        ? new URLSearchParams(window.location.search).get("filter")
+        : "",
+    };
+  },
+  methods: {
+    search() {
+      this.$emit("search-filter", this.keyword);
+    },
+    clearSearch() {
+      this.keyword = "";
+      this.$emit("search-filter", this.keyword);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -2,16 +2,23 @@ angular
   .module('appModule')
   .controller('homeController', homePageController);
 
-function homePageController(Employees) {
+function homePageController(Employees, $location) {
   const homePageVm = this;
   homePageVm.employees = [];
+  homePageVm.keyword = $location.search().filter ? $location.search().filter : '';
 
   activate();
 
   function activate() {
     Employees.getEmployees()
-      .then(({ data }) => {
+      .then(({
+        data,
+      }) => {
         homePageVm.employees = homePageVm.employees.concat(data.employees);
       });
   }
+
+  homePageVm.handleSearch = (keyword) => {
+    homePageVm.keyword = keyword;
+  };
 }
