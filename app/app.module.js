133fcbb36d346577a36c82ca16d6d167dd0b073e
ahmed <ahmedhali345@gmail.com>
2022-05-15 11:14:10 +0200

    add not found page

--- app/app.module.js
+++ app/app.module.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import 'ngVue';
 import 'ngVue/build/plugins.js';
 import PerformancePageComponent from './pages/performance-page.vue';
+import NotFoundPage from './pages/not-found.vue';
 import PerformanceChartComponent from './components/vue-components/performance-chart.vue';
 import SearchComponent from './components/vue-components/Search.vue';
 import store from './store/store.js';
@@ -24,6 +25,10 @@ angular.module('appModule').directive('vPerformancePage', (createVueComponent) =
 
 angular.module('appModule').directive('vPerformanceChart', (createVueComponent) => {
   return createVueComponent(Vue.component('performanceChartComponent', PerformanceChartComponent));
+});
+
+angular.module('appModule').directive('vNotFound', (createVueComponent) => {
+  return createVueComponent(Vue.component('NotFoundPage', NotFoundPage));
 });
 
 angular.module('appModule').directive('searchComponent', (createVueComponent) => {
---
+++ app/pages/not-found.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Error 404</h1>
+    <h4>Not Found Page</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
